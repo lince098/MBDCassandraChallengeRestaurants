d9b23081b78650812c089aaa77af017c737c5213
lince098 <pablogamarro@hotmail.com>
2022-12-22 20:35:49 +0100

    Cambio de posicion latitud por longitud

--- utils.py
+++ utils.py
@@ -13,10 +13,11 @@ class Vecindario:
         self.id = id
         self.polygons = []
         if tipo == 'Polygon':
-            self.addPolygon(puntos[0])
+            self.addPolygon(list(map(lambda x: list(reversed(x)), puntos[0])))
         else:
             for i in range(len(puntos)):
-                self.addPolygon(puntos[i][0])
+                self.addPolygon(
+                    list(map(lambda x: list(reversed(x)), puntos[i][0])))
 
     def addPolygon(self, puntos):
         self.polygons.append(mplPath.Path(np.array(puntos)))
@@ -32,24 +33,27 @@ class Restaurante:
     def __init__(self, id, location):
         self.id = id
         self.location = location
+        self.location.reverse()
 
 
 def dumpPolygon(neighborhood, dumpFile):
     for i in range(len(neighborhood["geometry"]["coordinates"][0])):
+        neighborhood["geometry"]["coordinates"][0][i].reverse()
         dumpLine = "{0},0,{1},{2},{3},\"{4}\"\n".format(neighborhood["_id"]["$oid"],
-                                                    i, neighborhood['name'],
-                                                    neighborhood["geometry"]["type"],
-                                                    neighborhood["geometry"]["coordinates"][0][i])
+                                                        i, neighborhood['name'],
+                                                        neighborhood["geometry"]["type"],
+                                                        neighborhood["geometry"]["coordinates"][0][i])
         dumpFile.write(dumpLine)
 
 
 def dumpMultiPolygon(neighborhood, dumpFile):
     for i in range(len(neighborhood["geometry"]["coordinates"])):
         for j in range(len(neighborhood["geometry"]["coordinates"][i][0])):
+            neighborhood["geometry"]["coordinates"][i][0][j].reverse()
             dumpLine = "{0},{1},{2},{3},{4},\"{5}\"\n".format(neighborhood["_id"]["$oid"],
-                                                          i, j, neighborhood['name'],
-                                                          neighborhood["geometry"]["type"],
-                                                          neighborhood["geometry"]["coordinates"][i][0][j])
+                                                              i, j, neighborhood['name'],
+                                                              neighborhood["geometry"]["type"],
+                                                              neighborhood["geometry"]["coordinates"][i][0][j])
             dumpFile.write(dumpLine)
 
 
